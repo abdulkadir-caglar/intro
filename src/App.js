49ac85bbe97024956f93ce1f50749b4a4bbefdb8
abdulkadir-caglar <110019810+abdulkadir-caglar@users.noreply.github.com>
2024-01-12 18:17:08 +0300

    props added to components

--- src/App.js
+++ src/App.js
@@ -5,20 +5,24 @@ import Product from './Product';
 import { Col, Container, Row } from 'reactstrap';
 
 function App() {
+  const naviInfo={title:"Navi Component"}
+  const categoryInfo = {title:"Category List"};
+  const productInfo = {title:"Product List"};
+
   return (
     <div>
       <Container>
         <Row>
-          <Navi />
+          <Navi info={naviInfo}/>
         </Row>
         
         <Row>
           <Col xs="3">
-          <Category />
+          <Category info={categoryInfo}/>
           </Col>
 
           <Col xs="9">
-          <Product />
+          <Product info={productInfo}/>
           </Col>
           
         </Row>
--- src/Category.js
+++ src/Category.js
@@ -2,10 +2,15 @@ import React, { Component } from "react";
 import { ListGroup, ListGroupItem } from "reactstrap";
 
 export default class Category extends Component {
+    constructor(props){
+        super(props);
+        state:{};
+    }
+    
     render() {
         return (
             <div>
-                <h2>Category List</h2>
+                <h2>{this.props.info.title}</h2>
                 <ListGroup>
                     <ListGroupItem>
                         Cras justo odio
