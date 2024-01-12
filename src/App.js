64ca0af339e11fd1e46f354e92df13164c2eb027
abdulkadir-caglar <110019810+abdulkadir-caglar@users.noreply.github.com>
2024-01-12 17:57:12 +0300

    bootstrap and reactsrap added to project

--- src/App.js
+++ src/App.js
@@ -2,13 +2,27 @@ import React from 'react';
 import Navi from './Navi';
 import Category from './Category';
 import Product from './Product';
+import { Col, Container, Row } from 'reactstrap';
 
 function App() {
   return (
     <div>
-      <Navi/>
-      <Category/>
-      <Product/>    
+      <Container>
+        <Row>
+          <Navi />
+        </Row>
+        
+        <Row>
+          <Col xs="3">
+          <Category />
+          </Col>
+
+          <Col xs="9">
+          <Product />
+          </Col>
+          
+        </Row>
+      </Container>
     </div>
   );
 }
