d8514e0d21164c159ccab53895211d9c3b0a3bd5
abdulkadir-caglar <110019810+abdulkadir-caglar@users.noreply.github.com>
2024-01-13 17:18:27 +0300

    statement object added to category component

--- src/Category.js
+++ src/Category.js
@@ -2,31 +2,33 @@ import React, { Component } from "react";
 import { ListGroup, ListGroupItem } from "reactstrap";
 
 export default class Category extends Component {
-    constructor(props){
+    constructor(props) {
         super(props);
-        state:{};
+        this.state = {
+            categories: [{
+                categoryId: 1,
+                categoryName: "Beverages"
+            },
+            {
+                categoryId: 2,
+                categoryName: "Condiments"
+            }
+            ]
+        };
     }
-    
+
     render() {
         return (
             <div>
                 <h2>{this.props.info.title}</h2>
                 <ListGroup>
-                    <ListGroupItem>
-                        Cras justo odio
-                    </ListGroupItem>
-                    <ListGroupItem>
-                        Dapibus ac facilisis in
-                    </ListGroupItem>
-                    <ListGroupItem>
-                        Morbi leo risus
-                    </ListGroupItem>
-                    <ListGroupItem>
-                        Porta ac consectetur ac
-                    </ListGroupItem>
-                    <ListGroupItem>
-                        Vestibulum at eros
-                    </ListGroupItem>
+                    {
+                        this.state.categories.map(category => (
+                            <ListGroupItem key={category.categoryId}>
+                                {category.categoryName}
+                            </ListGroupItem>
+                        ))
+                    }
                 </ListGroup>
             </div>
         )
